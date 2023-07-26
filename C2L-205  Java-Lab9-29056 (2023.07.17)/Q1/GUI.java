989abf0dee22576097496cb3160caeaaaf0842fa
chathuramd <135594601+chathuramd@users.noreply.github.com>
2023-07-27 01:07:31 +0530

    Add files via upload

---
+++ C2L-205  Java-Lab9-29056 (2023.07.17)/Q1/GUI.java
@@ -0,0 +1,12 @@
+package com.mycompany.gui;
+
+
+public class GUI 
+{
+
+    public static void main(String[] args) 
+    {
+        color n=new color();
+        n.show();
+    }
+}
---
+++ C2L-205  Java-Lab9-29056 (2023.07.17)/Q2/GUI1.java
@@ -0,0 +1,12 @@
+package com.mycompany.gui1;
+
+
+public class GUI1 
+{
+
+    public static void main(String[] args) 
+    {
+        Counter c=new Counter();
+        c.show();
+    }
+}
